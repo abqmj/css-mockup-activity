1c7e991109e4e559bb8f12e7f91450a518372649
Michael Jordan <mikesjordan@gmail.com>
2017-04-27 11:54:15 -0600

    changed title

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		<!-- custom CSS stylesheet -->
 		<link rel="stylesheet" href="css/style.css" type="text/css" />
 
-		<title>CSS Mockup Activity</title>
+		<title>Dylan McDonald AKA @deepdivedylan</title>
 	</head>
 	<body class="sfooter">
 		<div class="sfooter-content">
